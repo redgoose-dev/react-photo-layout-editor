c0b74c9b505a474953a29947535d3dd479aed73c
redgoose <scripter@me.com>
2017-07-30 11:34:09 +0900

    빌드 작은수정

--- webpack-build-pack.config.js
+++ webpack-build-pack.config.js
@@ -19,20 +19,7 @@ module.exports = {
 
 	devtool: false,
 
-	externals: {
-		"react": {
-			"commonjs": "react",
-			"commonjs2": "react",
-			"amd": "react",
-			"root": "React"
-		},
-		"react-dom": {
-			"commonjs": "react-dom",
-			"commonjs2": "react-dom",
-			"amd": "react-dom",
-			"root": "ReactDOM"
-		}
-	},
+	externals: {},
 
 	module: {
 		rules: [
